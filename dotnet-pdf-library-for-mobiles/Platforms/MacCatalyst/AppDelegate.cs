132a44c174f5559b42ef58604bd15ac861faba5c
Shantanu Methikar <smethikar18@gmail.com>
2024-06-03 17:48:45 +0530

    Delete Windows, Mac and Tizen folders

--- dotnet-pdf-library-for-mobiles/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace dotnet_pdf_library_for_mobiles;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
